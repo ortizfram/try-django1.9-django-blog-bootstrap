3fed21caf1252b41d47a5ad352b1a5dece3bc37e
ortizfram <ortizfranco48@gmail.com>
2023-08-28 13:23:42 -0300

    post view and urls init template

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    path("posts/", include("apps.posts.urls")),
     path('admin/', admin.site.urls),
 ]
