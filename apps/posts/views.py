9310f657b63b10cc3e44f84055495d167968777e
ortizfram <ortizfranco48@gmail.com>
2023-08-28 13:48:50 -0300

    templates

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -9,11 +9,11 @@ from . import views
 def post_create(request):
     return HttpResponse("<h1>create</h1>")
 
-def post_detail(request):
+def post_detail(request): # retrieve
     return HttpResponse("<h1>detail</h1>")
 
-def post_list(request):
-    return HttpResponse("<h1>list</h1>")
+def post_list(request): #list items
+    return render(request, "index.html", {})
 
 def post_update(request):
     return HttpResponse("<h1>update</h1>")
