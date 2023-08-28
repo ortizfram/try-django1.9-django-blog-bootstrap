8e960aa66130baa023be24e8c8a6696222fb0f29
ortizfram <ortizfranco48@gmail.com>
2023-08-28 12:07:41 -0300

    PostModelAdmin

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from apps.posts.models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title","updated","timestamp"]
+    list_display_link = ["updated"]
+    list_filter = ["updated","timestamp"]
+    search_fileds= ["title","content"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
