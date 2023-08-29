fce35d9ce25965f3e70678a9b6a267849d905600
melon <ortizfranco48@gmail.com>
2023-08-29 18:01:20 -0300

    add bootstrap

--- core/settings.py
+++ core/settings.py
@@ -113,6 +113,7 @@ STAICFILES_DIRS = [
 
 # CDN => content delivey network
 STATIC_ROOT = os.path.join(BASE_DIR,"static_cdn")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media_cdn")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
