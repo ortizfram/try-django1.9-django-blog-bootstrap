ad4967ca4eb15128b95a4e79f3356b7e6861d54d
ortizfram <ortizfranco48@gmail.com>
2023-08-29 09:29:05 -0300

    post detail url

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list),
-    path('create/', views.post_create),
-    path(r'^(detail/?P<id>\d+)/$', views.post_detail),
-    path('update/', views.post_update),
-    path('delete/', views.post_delete),
+    path('', views.post_list, name='post_list'),
+    path('create/', views.post_create, name='post_create'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('update/', views.post_update, name='post_update'),
+    path('delete/', views.post_delete, name='post_delete'),
 ]
