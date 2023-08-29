f0b4c197bbfdad6a50b443cc11f2371d45560a49
melon <ortizfranco48@gmail.com>
2023-08-29 17:51:05 -0300

    link static/css with posts app

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("posts/", include("apps.posts.urls")),
     path('admin/', admin.site.urls),
 ]
+
+# for online static
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
