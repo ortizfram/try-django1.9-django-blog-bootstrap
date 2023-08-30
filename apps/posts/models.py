3000ff32ee0f0e6296c66b83471858863d98b3ce
ortizfram <ortizfranco48@gmail.com>
2023-08-30 11:39:00 -0300

    slugify

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+
 # Create your models here.
 # MVC: MODEL VIEW CONTROLLER
 
@@ -7,7 +10,6 @@ def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
-    title = models.CharField(max_length=120)
     image = models.ImageField(upload_to=upload_location,
                               null=True,
                               blank=True, 
@@ -15,6 +17,8 @@ class Post(models.Model):
                               width_field="width_field")
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     excerpt = models.CharField(max_length=250)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
@@ -29,4 +33,24 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
+
+def create_slug(instance, new_slug=None): # recursive function
+    #=> slugify title if new. but if slug exists return it (when update)
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    #=> use create_slug() if slug field is empty
+    if not instance.slug :
+        instance.slug = create_slug(instance) 
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
+
     
