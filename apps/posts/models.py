3b2941a1c329f0eeb1f28de267d3ba427c8f15a9
ortizfram <ortizfranco48@gmail.com>
2023-09-01 10:25:44 -0300

    draft post in model and tempalte

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -23,6 +23,8 @@ class Post(models.Model):
     slug = models.SlugField(unique=True)
     excerpt = models.CharField(max_length=250)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
